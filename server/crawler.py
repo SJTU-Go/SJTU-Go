51094b15f96ac61de5ac928fcef9ffa57e95a4d0
ltzone <ltzhou@sjtu.edu.cn>
2020-03-26 22:52:09 +0800

    单车的批量爬取脚本+数据库读写

--- server/crawler.py
+++ server/crawler.py
@@ -3,6 +3,18 @@
 import requests
 import json
 import sys
+import pymysql
+import time
+
+token = "b035c3c414764f09ac9f687d288ce812",
+
+conn = pymysql.connect( host='127.0.0.1',
+                        port=3306,
+                        user='root',
+                        passwd='',
+                        db = 'hellobike',
+                        charset = 'utf8')
+crusor = conn.cursor()
 
 session = requests.session()
 
@@ -17,11 +29,45 @@ def hello_nearby(lat,lng):
       "currentLng":"121.334864",
       "currentLat":"31.156389",
       "adCode": "310112",
-      "token": "699fa0191f7041a18d0f0da1f6957d7b", # provided manually
+      "token": "b035c3c414764f09ac9f687d288ce812", # provided manually
       }
     req = requests.post(url,json=headers)
-    data = json.loads(req.text)
+    data = json.loads(req.text)['data']
     return data
 
+def write_db(bike):
+    values = [bike['bikeNo'], bike['lat'], bike['lng'], bike['price'][:-1],
+              bike['time'][:-2],  bike['bikeType'], bike['bikeColor']]
+    command = 'INSERT INTO 0326alltogether\
+               VALUES (%s, %s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP)'
+    crusor.execute(command, values)
+    conn.commit()
+    return 1
+
+
 if __name__ == '__main__':
-    print (hello_nearby(31.154295066535393,121.3361130759009))
+    tr_lat = 31.0349908800
+    tr_lng = 121.4459288100
+    bl_lat = 31.0163088100
+    bl_lng = 121.4278078100
+    br_lat = 31.0237195600
+    br_lng = 121.4505744000
+    x_block = 15
+    y_block = 8
+    
+    x_lat = (br_lat - bl_lat) / x_block
+    x_lng = (br_lng - bl_lng) / x_block
+    y_lat = (tr_lat - br_lat) / y_block
+    y_lng = (tr_lng - br_lng) / y_block
+
+
+    for i in range(x_block):
+        for j in range(y_block):
+            fetch = hello_nearby(bl_lat + i*x_lat + j*y_lat , bl_lng + i*x_lng + j*y_lng)
+            for item in fetch:
+                try:
+                    write_db(item)
+                except:
+                    print ('Error in',end="")
+                    print (item)
+            print ("finish ",(i,j)," block")
