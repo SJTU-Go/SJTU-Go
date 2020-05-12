15e5c2ad6ec3146ff2902f2fe304a04d814d4bf3
unknown <unknown@example.com>
2020-05-12 21:32:16 +0800

    5/12 21:32 更新日程界面，历史记录界面套用模板细节还未更改
    rt

--- miniprogram/pages/HistoryPage/HistoryPage.js
+++ miniprogram/pages/HistoryPage/HistoryPage.js
@@ -1,8 +1,39 @@
 //index.js
 const app = getApp()
-
 Page({
-  navigatePage:function()
-  {wx.switchTab({url: '../SearchPage/SearchPage', })},
-
+  data:{},
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    this.setData({
+       orderList:[{
+        order_id:1,
+        order_no:'SA02201703052988',
+        money:"xxx",
+        imgs:['/assets/testimages/timg1.jpg','/assets/testimages/timg1.jpg','/assets/testimages/timg1.jpg'],
+        customer_name:'xxx',
+        order_status:'8:00'
+       },{
+        order_id:2,
+        order_no:'SA02201703052988',
+        create_time:'2017.03.05 18:30',
+        imgs:['/assets/testimages/timg1.jpg','/assets/testimages/timg1.jpg','/assets/testimages/timg1.jpg'],
+        customer_name:'xxx',
+        money:"xxx",
+        order_status:'7：20'
+       }]
+    })
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
 })
+
--- miniprogram/pages/schedulePage/schedulePage.js
+++ miniprogram/pages/schedulePage/schedulePage.js
@@ -1,9 +1,123 @@
 //index.js
 const app = getApp()
-
+var util = require("../../utils/util.js")
 Page({
-
-  navigatePage:function()
-  {wx.switchTab({url: '../SearchPage/SearchPage', })},
-
+ 
+  /**
+   * 页面的初始数据
+   * selectWeek 0代表的本周  1代表下一周  -1代表上一周   
+   * timeBean 传递给组件的数据，数据的格式在一开始的工具类中明确
+   */
+  data: {
+    selectWeek:0,
+    timeBean:{},
+    mHidden: true,
+    selectShow: false,//控制下拉列表的显示隐藏，false隐藏、true显示
+    selectShow2: false,//控制下拉列表的显示隐藏，false隐藏、true显示
+    selectData: ['0时','1时','2时','3时','4时','5时','6时','7时','8','9'],//下拉列表的数据
+    selectData2: ['0fen','1fen','2时','3时','4时','5时','6时','7时','8','9'],
+    index: 0,//选择的下拉列表下标
+    index2: 0,//选择的下拉列表下标    
+  },
+ 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+ 
+  },
+  addschedule:function(){
+  this.setData({mHidden:false})
+  },
+  modalcancelled:function(){
+    this.setData({mHidden:true})
+    },
+  changeModal:function(){
+    console.log(this.data.index)
+    console.log(this.data.index2) 
+    console.log(this.data.timeBean)   
+    this.setData({mHidden:true})
+  },
+  /**
+   * 点击了上一周，选择周数字减一，然后直接调用工具类中一个方法获取到数据
+   */
+  lastWeek:function(e){   
+    var selectWeek = --this.data.selectWeek;
+    var timeBean = this.data.timeBean
+    timeBean = util.getWeekDayList(selectWeek)
+ 
+    if (selectWeek != 0) {
+      timeBean.selectDay = 0;
+    }
+ 
+    this.setData({
+      timeBean,
+      selectWeek
+    })
+  },
+ 
+  /**
+   * 点击了下一周，选择周数字加一，然后直接调用工具类中一个方法获取到数据
+   */
+  nextWeek:function(e){
+    var selectWeek = ++this.data.selectWeek;
+    var timeBean = this.data.timeBean
+    timeBean = util.getWeekDayList(selectWeek)
+ 
+    if (selectWeek != 0){
+      timeBean.selectDay = 0;
+    }
+ 
+    this.setData({
+      timeBean,
+      selectWeek
+    })
+  },
+ 
+  /**
+   * 选中了某一日，改变selectDay为选中日
+   */ 
+  dayClick:function(e){
+    var timeBean = this.data.timeBean
+    timeBean.selectDay = e.detail;
+    this.setData({
+      timeBean,
+    })
+    console.log(this.data.timeBean)
+  },
+ 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    this.setData({
+      timeBean: util.getWeekDayList(this.data.selectWeek)
+    })
+  },
+  selectTap() {
+    this.setData({
+      selectShow: !this.data.selectShow
+    });
+  },
+  selectTap2() {
+    this.setData({
+      selectShow2: !this.data.selectShow2
+    });
+  },
+  // 点击下拉列表
+  optionTap(e) {
+    let Index = e.currentTarget.dataset.index;//获取点击的下拉列表的下标
+    this.setData({
+      index: Index,
+      selectShow: !this.data.selectShow
+    });
+  },
+  optionTap2(e) {
+    let Index2 = e.currentTarget.dataset.index;//获取点击的下拉列表的下标
+    this.setData({
+      index2: Index2,
+      selectShow2: !this.data.selectShow2
+    });
+  }
+ 
 })
