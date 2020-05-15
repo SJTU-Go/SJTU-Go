254936163fd18d3d3eda4660273f94d038705086
ltzone <ltzhou@sjtu.edu.cn>
2020-05-15 15:53:55 +0800

    实体类配置，对接已写入的mapvertex,destination数据表

--- src/main/java/org/sjtugo/api/DAO/MapVertexInfo.java
+++ src/main/java/org/sjtugo/api/DAO/MapVertexInfo.java
@@ -3,28 +3,33 @@ package org.sjtugo.api.DAO;
 import com.vividsolutions.jts.geom.Point;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.awt.*;
 
 @Data
 @Entity
+@Table(name = "map_vertex_info")
 public class MapVertexInfo {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="vertexid")
     private Integer vertexID;
 
+    @Column(name = "vertex_name")
     private String vertexName;
 
+    @Column(name = "location")
     private Point location;
 
+    @Column(name="bike_count")
     private int bikeCount;
 
+    @Column(name="motor_count")
     private int motorCount;
 
+    @Column(name="park_info")
     private String parkInfo;
 
+    @Column(name="park_size")
     private String parkSize;
 }
