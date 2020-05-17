8383c4cac468b8ea35dfc71ca7d19b2e535a340a
ltzone <ltzhou@sjtu.edu.cn>
2020-05-17 15:19:50 +0800

    fix api specs

--- src/main/java/org/sjtugo/api/controller/NavigateControl.java
+++ src/main/java/org/sjtugo/api/controller/NavigateControl.java
@@ -84,17 +84,18 @@ public class NavigateControl {
     @Data
     static class NavigateRequest {
         @ApiModelProperty(value = "出发点ID或经纬度", required = true,
-                example = "DT000341")
+                example = "图书馆")
         private String beginPlace;
         @ApiModelProperty(value = "到达点ID或经纬度", required = true,
-                example = "POINT (121.435624 31.234566)")
+                example = "POINT (121.435505 31.026303)")
         private String arrivePlace;
         @ApiModelProperty(value = "途径点ID或经纬度",
-                example = "[\"第三餐饮大楼\", \"POINT (121.435624 31.234566)\"]")
+                example = "[\"学生服务中心\"]")
         private List<String> passPlaces;
 
 
-        @ApiModelProperty(value = "出发时间", example = "2020/05/11 12:05:12")
+        @ApiModelProperty(value = "出发时间（必须严格按照格式，不足位用0补齐），可不设该字段，默认为当前时间",
+                example = "2020/05/11 12:05:12")
         @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy/MM/dd HH:mm:ss", timezone="GMT+8")
         @JsonSerialize(using = LocalDateTimeSerializer.class)
