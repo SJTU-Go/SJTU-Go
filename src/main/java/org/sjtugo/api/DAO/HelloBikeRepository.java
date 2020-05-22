10bcce2a66ecef0701311b80a10a8883fe357119
ltzone <ltzhou@sjtu.edu.cn>
2020-05-22 18:07:56 +0800

    完善查询单车信息的功能‘ ’

--- src/main/java/org/sjtugo/api/DAO/HelloBikeRepository.java
+++ src/main/java/org/sjtugo/api/DAO/HelloBikeRepository.java
@@ -1,11 +1,30 @@
 package org.sjtugo.api.DAO;
 
+import com.vividsolutions.jts.geom.Polygon;
 import org.sjtugo.api.entity.HelloBikeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.List;
 
-@Table(name = "helloBikeInfo")
 public interface HelloBikeRepository extends JpaRepository<HelloBikeInfo,Integer> {
-    int countAllByClusterPointEquals(String a);
+    @Query( value = "SELECT COUNT(*) FROM helloBikeInfo WHERE TIMESTAMPDIFF(SECOND , CURRENT_TIMESTAMP, time) < 120" +
+            " AND cluster_point = :id",
+            nativeQuery = true)
+    int findBikeCounts(@Param("id") String id);
+
+//    @Query( value = "SELECT u FROM HelloBikeInfo u WHERE ABS(u.lng - :longi) < 0.002 " +
+//            "AND ABS(u.lat - :lati)< 0.002")
+//    List<HelloBikeInfo> findNearbyBikes(@Param("longi") double longi, @Param("lati") double lati); //, @Param("time")LocalDateTime time);
+
+    List<HelloBikeInfo> findAllByLngBetweenAndLatBetweenAndTimeAfter(double lng1,double lng2,double lat1,double lat2, LocalDateTime time);
+
+
+//    @Query( value = "SELECT u FROM HelloBikeInfo u")
+//    List<HelloBikeInfo> findNearbyBikes(); //, @Param("time")LocalDateTime time);
+
+
 }
--- src/main/java/org/sjtugo/api/entity/HelloBikeInfo.java
+++ src/main/java/org/sjtugo/api/entity/HelloBikeInfo.java
@@ -2,28 +2,51 @@ package org.sjtugo.api.entity;
 
 import com.bedatadriven.jackson.datatype.jts.serialization.GeometryDeserializer;
 import com.bedatadriven.jackson.datatype.jts.serialization.GeometrySerializer;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Point;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
-
+@Table(name="helloBikeInfo")
 public class HelloBikeInfo {
     @Id
     @Column(name = "BikeID")
+    @ApiModelProperty(value = "bikeID", example = "")
     private String bikeID;
 
     @Column(name="time")
+    @JsonFormat(pattern="HH:mm:ss", timezone="GMT+8")
     private LocalDateTime time;
 
     @Column(name="cluster_point")
     private String clusterPoint;
+
+//    @Transient
+//    @JsonSerialize(using = GeometrySerializer.class)
+//    @JsonDeserialize(contentUsing = GeometryDeserializer.class)
+//    private Point location;
+
+    @Column(name="bikeType")
+    private int bikeType;
+
+    @Column(name="longitude")
+    private double lng;
+
+    @Column(name="latitude")
+    private double lat;
+//    public Point getLocation(){
+//        return new GeometryFactory().createPoint(new Coordinate(lng,lat));
+
+
+//    }
 }
