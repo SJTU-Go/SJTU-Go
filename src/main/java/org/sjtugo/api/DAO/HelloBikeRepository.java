864751a0b50bf3708c478b7002746fa28e7701d6
ltzone <ltzhou@sjtu.edu.cn>
2020-06-12 17:38:16 +0800

    优化查询

--- src/main/java/org/sjtugo/api/DAO/HelloBikeRepository.java
+++ src/main/java/org/sjtugo/api/DAO/HelloBikeRepository.java
@@ -15,7 +15,9 @@ public interface HelloBikeRepository extends JpaRepository<HelloBikeInfo,Integer
     int findBikeCounts(@Param("id") String id);
 
     @Query( value = "SELECT *, ST_LENGTH(LineString(POINT(longitude,latitude),POINT(:longi,:lati))) AS distance" +
-            " FROM hello_bike_info WHERE ABS(longitude - :longi) < 0.002 " +
+            " FROM hello_bike_info WHERE " +
+            "longitude < 0.002 + :longi AND :longi - 0.002 < longitude " +
+            "AND latitude  < :lati + 0.002 AND latitude > :lati - 0.002 " +
             "AND ABS(latitude - :lati)< 0.002 AND TIMESTAMPDIFF(SECOND,time,current_timestamp) < 300" +
             " ORDER BY distance LIMIT 30",
             nativeQuery = true)
