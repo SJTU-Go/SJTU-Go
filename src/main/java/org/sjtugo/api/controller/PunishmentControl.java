99a0641aa710849eedb26cb683645f45fd3ce69e
ltzone <ltzhou@sjtu.edu.cn>
2020-07-24 00:12:44 +0800

    expect data of trips

--- src/main/java/org/sjtugo/api/controller/PunishmentControl.java
+++ src/main/java/org/sjtugo/api/controller/PunishmentControl.java
@@ -1,25 +1,13 @@
 package org.sjtugo.api.controller;
 
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
 import io.swagger.annotations.*;
 import lombok.Data;
-import net.sf.json.JSONObject;
-import org.sjtugo.api.DAO.CarInfoRepository;
-import org.sjtugo.api.DAO.DestinationRepository;
-import org.sjtugo.api.entity.Punishment;
 import org.sjtugo.api.DAO.PunishmentRepository;
-import org.sjtugo.api.DAO.Entity.CarInfo;
-import org.sjtugo.api.DAO.Entity.Destination;
-import org.sjtugo.api.DAO.Entity.HelloBikeInfo;
-import org.sjtugo.api.DAO.Entity.MapVertexInfo;
-import org.sjtugo.api.DAO.HelloBikeRepository;
 import org.sjtugo.api.DAO.MapVertexInfoRepository;
-import org.sjtugo.api.service.MapInfoService;
+import org.sjtugo.api.DAO.TripRepository;
 import org.sjtugo.api.service.PunishmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -36,10 +24,13 @@ public class PunishmentControl {
     private MapVertexInfoRepository mapVertexInfoRepository;
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private TripRepository tripRepository;
+
     @ApiOperation(value = "记录惩罚")
     @PostMapping("/punish")
     public ResponseEntity<?> punishmentadd(@RequestBody PunishmentRequest punishmentRequest){
-        PunishmentService punisher = new PunishmentService(mapVertexInfoRepository,punishmentRepository);
+        PunishmentService punisher = new PunishmentService(mapVertexInfoRepository,punishmentRepository, tripRepository, restTemplate);
         return punisher.addpunishment(punishmentRequest.getPunishment());
     }
     @Data
--- src/main/java/org/sjtugo/api/service/PunishmentService.java
+++ src/main/java/org/sjtugo/api/service/PunishmentService.java
@@ -1,36 +1,28 @@
 package org.sjtugo.api.service;
-import net.sf.json.JSONArray;
 import org.sjtugo.api.DAO.Entity.MapVertexInfo;
-import org.sjtugo.api.entity.Punishment;
-import org.sjtugo.api.entity.TimeStamp;
+import org.sjtugo.api.DAO.TripRepository;
+import org.sjtugo.api.entity.*;
 import org.sjtugo.api.DAO.PunishmentRepository;
 import org.sjtugo.api.DAO.MapVertexInfoRepository;
-import net.sf.json.JSONObject;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.sjtugo.api.DAO.UserRepository;
-import org.sjtugo.api.entity.User;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
 
-import javax.validation.constraints.Null;
-import java.sql.Time;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PunishmentService {
     private final PunishmentRepository punishmentRepository;
     private final MapVertexInfoRepository mapVertexInfoRepository;
-    public PunishmentService(MapVertexInfoRepository mapVertexInfoRepository,PunishmentRepository punishmentRepository){
+    private final TripRepository tripRepository;
+    private final RestTemplate restTemplate;
+
+
+    public PunishmentService(MapVertexInfoRepository mapVertexInfoRepository, PunishmentRepository punishmentRepository, TripRepository tripRepository, RestTemplate restTemplate){
         this.punishmentRepository = punishmentRepository;
         this.mapVertexInfoRepository = mapVertexInfoRepository;
+        this.tripRepository = tripRepository;
+        this.restTemplate = restTemplate;
     }
     public ResponseEntity<?> addpunishment(List<List<Double>> punishment){
         List<TimeStamp> timcomputed = new ArrayList<TimeStamp>();
@@ -71,5 +63,71 @@ public class PunishmentService {
 
 
         return new ResponseEntity<>("userInfo", HttpStatus.OK);
+    }
+
+
+    private static final List<String> recordTrafficType = Arrays.asList("BIKE", "E100", "MOTOR");
+
+    @SuppressWarnings("unchecked")
+    private List<TimeStamp> findTripExpectTime(Integer tripID, Integer segIndex){
+        Trip curTrip = tripRepository.findById(tripID).orElseThrow();
+        List<List<Integer>> curRoutes = ((List<LinkedHashMap<String,Object>>) curTrip.getStrategy().get("routePlan"))
+                                .stream().filter(item -> recordTrafficType.contains((String) item.get("type")))
+                                .map(item -> (List<Integer>) item.get("passingVertex")).collect(Collectors.toList());
+
+        List<Integer> curRoute = curRoutes.get(segIndex);
+        List<TimeStamp> result = new ArrayList<>();
+        int stamp = 0;
+        for (int j = 0; j < curRoute.size()-1; j++) {
+            TimeStamp tmpStamp = new TimeStamp();
+            stamp += findEdgeTime(curRoute.get(j), curRoute.get(j + 1), 0);
+            // TODO: updateType
+            tmpStamp.setStamp(stamp);
+            tmpStamp.setVertexid(curRoute.get(j));
+            result.add(tmpStamp);
+        }
+        return result;
+    }
+
+    private Double findEdgeTime(Integer startID, Integer endID, Integer updateType){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization","bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjEuNTkxNTI5OTc2NDQ5Nzc1M2UrNiwiZXhwIjoxNTk0MTIxOTc2LCJpc3MiOiJhcmFuZ29kYiIsInByZWZlcnJlZF91c2VybmFtZSI6InJvb3QifQ==.UElwRx6Iy9yvT2gvX2rdCjlLnc73E56RfV6hEQd1sLA=");
+
+        String query = "";
+        switch (updateType){
+            case 0:
+                query = "FOR edge IN bikeedge\n" +
+                        "FILTER edge._from == \"vertex/" + startID.toString() +"\"\n" +
+                        "AND edge._to == \"vertex/" + endID.toString() +"\"\n" +
+                        "COLLECT t = edge.normalBikeTime\n" +
+                        "RETURN t";
+                break;
+            case 1:
+                query = "FOR edge IN bikeedge\n" +
+                        "FILTER edge._from == \"vertex/" + startID.toString() +"\"\n" +
+                        "AND edge._to == \"vertex/" + endID.toString() +"\"\n" +
+                        "COLLECT t = edge.normalMotorTime\n" +
+                        "RETURN t";
+                break;
+            case 2:
+                query = "FOR edge IN caredge\n" +
+                        "FILTER edge._from == \"vertex/" + startID.toString() +"\"\n" +
+                        "AND edge._to == \"vertex/" + endID.toString() +"\"\n" +
+                        "COLLECT t = edge.normalCarTime\n" +
+                        "RETURN t";
+            default:
+                break;
+        }
+
+        Map<String,Object> bindVars = new HashMap<>();
+        bindVars.put("query", query);
+        bindVars.put("batchSize", 1);
+
+        HttpEntity<Object> update_request = new HttpEntity<>(bindVars,headers);
+        Map<String,Object> result = (Map<String, Object>) restTemplate
+                                    .postForObject("http://47.92.147.237:8529/_api/cursor",
+                                                    update_request,Object.class);
+        return ((List<Double>) result.get("result")).get(0);
     }
 }
