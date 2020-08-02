38623467eb6743690c3f6932b3d48a5811eeb421
liaaabegin <1249529628@qq.com>
2020-08-02 21:46:35 +0800

    热度

--- crawler/popularity.py
+++ crawler/popularity.py
@@ -19,7 +19,7 @@ def get_info(db, sql):
         db.close()
 
     except:
-        db.ping()
+        db.ping(True)
         cursor = db.cursor()
         cursor.execute(sql)
         if sql[0:6].lower() == "select":
@@ -39,13 +39,13 @@ db = pymysql.connect(
 )
 
 # 得到4分钟内开始的行程，这个时候对应结束停车点热度要+5
-sql1 = "select strategy from playground.trip where depart_time > DATE_SUB(NOW(),INTERVAL 4 MINUTE);"
+sql1 = "select strategy from playground.trip where depart_time >= DATE_SUB(NOW(),INTERVAL 4 MINUTE);"
 
 # 得到4分钟内结束的行程，这个时候开始的停车点热度要+5
-sql2 = "select strategy from playground.trip where arrive_time > DATE_SUB(NOW(),INTERVAL 4 MINUTE);"
+sql2 = "select strategy from playground.trip where arrive_time >= DATE_SUB(NOW(),INTERVAL 4 MINUTE);"
 
 # 得到四分钟内搜索情况
-sql3 = "select search_names from playground.search_history where search_time > DATE_SUB(NOW(),INTERVAL 4 MINUTE);"
+sql3 = "select parkingid from playground.search_history where search_time >= DATE_SUB(NOW(),INTERVAL 4 MINUTE);"
 
 
 def get_popularity():
@@ -54,35 +54,47 @@ def get_popularity():
     search_h = get_info(db, sql3)  # 得到search_history
 
     for s in strategy_d:    # s是tuple类型
-        depart = s[0].split(",")[1]
-        depart_p = depart[10:-1]
-        #print(depart_p)
-        
-        sql5 = "update playground.map_vertex_info set popularity = popularity+5 where vertex_name = '{}' and vertexid >0".format(depart_p)
-        get_info(db, sql5)
+        p = s[0].replace('true', 'True')
+        # print(p)
+        depart = eval(p)['routeplan'][-1]
+        # eval将字符串转化为字典，从routeplan中获取信息,routeplan是列表
+        # print(type(depart))
+        depart_p = depart['departID'][2:]
+        print(depart_p)
+        sql4 = "update playground.map_vertex_info set popularity = popularity+5 where vertexid = '{}' ".format(depart_p)
+        get_info(db, sql4)
 
     for s in strategy_a:
-        arrive = s[0].split(",")[2]
-        arrive_p = arrive[10:-1]
+        p = s[0].replace('true', 'True')
+        arrive = eval(p)['routeplan'][-1]
 
-        sql5 = "update playground.map_vertex_info set popularity = popularity+5 where vertex_name = '{}' and vertexid >0".format(arrive_p)
+        arrive_p = arrive['arriveID'][2:]
+        print(arrive_p)
+        sql5 = "update playground.map_vertex_info set popularity = popularity+5 where vertexid = '{}'".format(arrive_p)
         get_info(db, sql5)
 
-    for s in search_h:  # s是搜索开始地点和结束地点的结合，由，隔开
-        begin = s[0].split(" ")[0]  # ？？前端传入的不一定是地点名？？
-        arrive = s[0].split(" ")[1]
+    for s in search_h:  # s是搜索对应停车点id
+        # begin = s[0].split(" ")[0]  # ？？前端传入的不一定是地点名？？
+        # arrive = s[0].split(" ")[1]
 
-        sql4 = "update playground.map_vertex_info set popularity = popularity+1 where vertex_name = '{}' and vertexid >0".format(begin)
-        get_info(db, sql4)
-        sql5 = "update playground.map_vertex_info set popularity = popularity+1 where vertex_name = '{}' and vertexid >0".format(arrive)
-        get_info(db, sql5)
+        print(type(s))
+        sql6 = "update playground.map_vertex_info set popularity = popularity+1 where vertexid = '{}' ".format(s)
+        get_info(db, sql6)
+
     print("最近更新时间："+str(datetime.now()))
+
+
+def reset():
+    sql = "update playground.map_vertex_info set popularity=0 where vertex_name is not null and vertexid >=0;"
+    get_info(db, sql)
+    print("更新")
 
 
 # 每1分钟执行一次
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
     scheduler.add_job(get_popularity, 'interval', minutes=1, next_run_time=datetime.now())
+    scheduler.add_job(reset, 'interval', minutes=4)
     # 还可以设置开始结束时间(..., start_date='2017-9-8 21:30:00', end_date='2018-06-15 21:30:00')
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))  # os.name返回操作系统类型
     try:
